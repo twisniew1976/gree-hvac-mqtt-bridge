4873641291ee9058717cc3313503dbc23964277f
Wisniox <twisniew1976@users.noreply.github.com>
2024-07-07 18:14:28 +0200

    V2 protocol support

--- app/deviceFactory.js
+++ app/deviceFactory.js
@@ -37,6 +37,10 @@ class Device {
          * @property {object} props - Properties
          */
     this.device = {}
+    this.encryptionVersion = 1;
+    this.tag = '';
+    this.packetSentNo = 0;
+    this.packetReceivedNo = 0;
 
     // Initialize connection and bind with device
     this._connectToDevice(this.options.host)
@@ -54,7 +58,10 @@ class Device {
       socket.bind(() => {
         const message = Buffer.from(JSON.stringify({ t: 'scan' }))
 
-        socket.setBroadcast(true)
+        this.packetSentNo++;
+        //console.log('[UDP] Sent[%d]: %s', this.packetSentNo, message);
+
+        socket.setBroadcast(true)        
         socket.send(message, 0, message.length, 7000, address)
 
         console.log('[UDP] Connected to device at %s', address)
@@ -84,7 +91,7 @@ class Device {
     this.device.bound = false
     this.device.props = {}
 
-    console.log('[UDP] New device registered: %s', this.device.name)
+    console.log('[UDP] New device registered: %s @ %s', this.device.id, this.device.address)
   }
 
   /**
@@ -92,21 +99,49 @@ class Device {
      * @param {Device} device Device object
      */
   _sendBindRequest (device) {
+    
+    let encryptedBoundMessage;
+
     const message = {
       mac: this.device.id,
       t: 'bind',
       uid: 0
     }
-    const encryptedBoundMessage = encryptionService.encrypt(message)
-    const request = {
+
+    if(this.encryptionVersion === 1) {
+      encryptedBoundMessage = encryptionService.encrypt(message)
+      this.tag = '';
+    }
+    else if(this.encryptionVersion === 2) {
+      const encrypted = encryptionService.encrypt_v2(message)
+      encryptedBoundMessage = encrypted.pack
+      this.tag = encrypted.tag;
+    }
+
+    const request = (this.tag === '') ? {
+      tcid: this.device.id,
       cid: 'app',
       i: 1,
       t: 'pack',
       uid: 0,
       pack: encryptedBoundMessage
+    } : {
+      tcid: this.device.id,
+      cid: 'app',
+      i: 1,
+      t: 'pack',
+      uid: 0,
+      tag: this.tag,
+      pack: encryptedBoundMessage
     }
+
     const toSend = Buffer.from(JSON.stringify(request))
     socket.send(toSend, 0, toSend.length, device.port, device.address)
+
+    this.packetSentNo++;
+    //console.log('[UDP] Sent[%d]: %s', this.packetSentNo, toSend);
+    
+    //console.log('[UDP] Device %s bind request sent...', this.device.id)
   }
 
   /**
@@ -117,7 +152,7 @@ class Device {
   _confirmBinding (id, key) {
     this.device.bound = true
     this.device.key = key
-    console.log('[UDP] Device %s is bound!', this.device.name)
+    console.log('[UDP] Device %s is bound!', this.device.id)
   }
 
   /**
@@ -141,20 +176,51 @@ class Device {
      * @param {number} rinfo.port Port number
      */
   _handleResponse (msg, rinfo) {
+    
+    this.packetReceivedNo++;
+
+    let pack;
     const message = JSON.parse(msg + '')
 
+    //console.log('[UDP] Received[%d]: %s', this.packetReceivedNo, message);
+
     // Extract encrypted package from message using device key (if available)
-    const pack = encryptionService.decrypt(message, (this.device || {}).key)
+    if (this.encryptionVersion === 1) {
+      //console.log('[UDP] Received packet with encryption version: 1')
+      pack = encryptionService.decrypt(message, message.i === 1 ? undefined : this.device.key)
+    }
+    else if (this.encryptionVersion === 2) {
+      //console.log('[UDP] Received packet with encryption version: 2')
+      pack = encryptionService.decrypt_v2(message, message.i === 1 ? undefined : this.device.key)
+    }
+
+    //console.log('[UDP] Pack %s', pack);
 
     // If package type is response to handshake
     if (pack.t === 'dev') {
-      this._setDevice(message.cid, pack.name, rinfo.address, rinfo.port)
+
+      //console.log('[UDP] Device respond for scan');
+
+      if (this.encryptionVersion === 1 && pack.ver && pack.ver.toString().startsWith('V2.')) {
+        //console.log('[UDP] Encryption switched to version: 2')
+        // first V2 version responded to scan command with V1 encryption but binding requires V2 encryption
+        this.encryptionVersion = 2;
+        this._setDevice(pack.cid, pack.name, rinfo.address, rinfo.port)
+      }
+      else {
+        this._setDevice(message.cid, pack.name, rinfo.address, rinfo.port)
+      }
+
       this._sendBindRequest(this.device)
       return
+
     }
 
     // If package type is binding confirmation
     if (pack.t === 'bindok' && this.device.id) {
+
+      //console.log('[UDP] Device respond for bind');
+
       this._confirmBinding(message.cid, pack.key)
 
       // Start requesting device status on set interval
@@ -173,9 +239,10 @@ class Device {
     }
 
     // If package type is response, update device properties
-    if (pack.t === 'res' && this.device.bound) {
+    if (pack.t === 'res' && this.device.bound) {      
       pack.opt.forEach((opt, i) => {
-        this.device.props[opt] = pack.val[i]
+        const value = pack.p !== undefined ? pack.p[i] : pack.val[i];
+        this.device.props[opt] = value;
       })
       this.options.onUpdate(this.device)
       return
@@ -208,16 +275,43 @@ class Device {
      * @param {number} [port] Port number
      */
   _sendRequest (message, address = this.device.address, port = this.device.port) {
-    const encryptedMessage = encryptionService.encrypt(message, this.device.key)
-    const request = {
+
+    let encryptedBoundMessage;
+
+    //console.log('[UDP] SendRequest: %s', message);  
+
+    if(this.encryptionVersion === 1) {
+      encryptedBoundMessage = encryptionService.encrypt(message, this.device.key)
+      this.tag = '';
+    }
+    else if(this.encryptionVersion === 2) {
+      const encrypted = encryptionService.encrypt_v2(message, this.device.key)
+      encryptedBoundMessage = encrypted.pack
+      this.tag = encrypted.tag;
+    }
+
+    const request = (this.tag === '') ? {
+      tcid: this.device.id,
       cid: 'app',
       i: 0,
       t: 'pack',
       uid: 0,
-      pack: encryptedMessage
+      pack: encryptedBoundMessage
+    } : {
+      tcid: this.device.id,
+      cid: 'app',
+      i: 0,
+      t: 'pack',
+      uid: 0,
+      tag: this.tag,
+      pack: encryptedBoundMessage
     }
     const serializedRequest = Buffer.from(JSON.stringify(request))
     socket.send(serializedRequest, 0, serializedRequest.length, port, address)
+
+    this.packetSentNo++;
+    //console.log('[UDP] Sent[%d]: %s', this.packetSentNo, serializedRequest);    
+
   };
 
   /**
